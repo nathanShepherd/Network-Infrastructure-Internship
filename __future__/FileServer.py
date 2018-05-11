c55a29e123e0920ce711ca843073e9d2cdd1d149
Nathan Shepherd <nateshep@localhost.localdomain>
2018-05-10 23:46:43 -0400

    Initial commit

--- __future__/FileServer.py
+++ __future__/FileServer.py
@@ -8,20 +8,33 @@ import threading
 def getFile(name, sock):
     filename = sock.recv(1024)
     if os.path.isfile(filename):
-        sock.send("EXISTS " + str(os.path.getsize(filename)))
+        sock.send(str.encode("Requested file exists " +\
+                              str(os.path.getsize(filename))))
+
         userResp = sock.recv(1024)
         if userResp[:2] == 'OK':
+
+	    # After client confirmation, send file
             with open(filename, 'rb') as f:
                 bytesToSend = f.read(1024)
                 sock.send(bytesToSend)
-    else:
-        sock.send('ERROR:\"%s\" does not exist' %s (filename) )
+
+                while bytesToSend != "":
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+    else: 
+        # Requested file does not exist
+        #sock.send(str.encode('ERROR:\"%s\" does not exist' %s (filename) ))
+        print('Requested file does not exist')
+
     sock.close()
 
 if __name__ == '__main__':
+    # Server Location
     host = '127.0.0.1'
     port = 5000
-
+    
+    # Initialize TCP Connection
     s = socket.socket()
     s.bind((host, port))
     s.listen(5)
@@ -29,10 +42,12 @@ if __name__ == '__main__':
     print('Server Started')
 
     while True:
+	#Start new thread iff a new client connects
         c, addr = s.accept()
-        print('Client connected to ip: ' + str(addr))
+        print('Client connected via ip: ' + str(addr))
         t = threading.Thread(target=getFile, args=('retrThread', c))
         t.start()
 
     s.close()
     
+
--- __future__/client.py
+++ __future__/client.py
@@ -4,32 +4,49 @@ import socket
 if __name__ == "__main__":
     host = '127.0.0.1'
     port = 5000
-
+    
+    # Connect to server
     s = socket.socket()
     s.connect((host, port))
 
     filename = input("Enter a filename: ")
     if filename not in ['quit', 'q', 'exit']:
+
         s.sendall(filename.encode('utf-8'))
+
         data = s.recv(1024)
-        if data[:6] == 'EXISTS':
-            filesize = float(data[6:])
-            message = input('File Exists, size is', str(filesize),
-                            'Bytes\n Download (y/n)? ')
+        if data[:5] != 'ERROR':
+
+            filesize = float((data.split(b" ")[-1]))
+            message = input('File Exists, size is ' + str(filesize) +\
+                            ' Bytes\n Download? (y/n): ')
+
             if message.lower() == 'y':
+		# Communicate to server that file exists
                 s.send(b'OK')
-                f = open('new_'+filename, 'wb')
+		
+		# initialize the new file to build locally
+                f = open('new_'+filename.split('/')[-1], 'wb')
+
+		# Receive the first batch of file information
                 data = s.recv(1024)
-                totalRecv = len(data)
+                totalBytesReceived = len(data)
                 f.write(data)
-                while totalRecv < filesize:
+
+		# Iteratively build the file until 
+                #while totalBytesReceived < filesize:
+                while data:
+                    print(totalBytesReceived, '|', filesize, '% done')
+                    #print('Downloading file ...')
                     data = s.recv(1024)
-                    totalRecv += len(data)
-                    f.write(date)
-                    print('{0:.2f}% done'.format((totalRecv/filesize*100)))
+                    totalBytesReceived += len(data)
+                    f.write(data)
+
                 print('Download Complete!')
 
         else:
             print('File does not exist.')
 
     s.close()
+
+#?
